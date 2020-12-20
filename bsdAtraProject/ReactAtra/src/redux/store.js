51650692251a543d651427350a3b6d5c5a05bc80
TamiPines <64363390+TamiPines@users.noreply.github.com>
2020-12-20 13:42:59 +0200

    20/12/tami

--- bsdAtraProject/ReactAtra/src/redux/store.js
+++ bsdAtraProject/ReactAtra/src/redux/store.js
@@ -1,11 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from './reducers';
 import thunk from 'redux-thunk';
 
 const store = createStore(
     reducers,
-    composeWithDevTools(applyMiddleware(thunk))
+    // composeWithDevTools(
+        applyMiddleware(thunk)
+        // )
 
 )
 
